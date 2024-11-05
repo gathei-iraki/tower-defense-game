ff0f8972c37e9ff9b153269b7eaa15d216adcea3
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-11-05 18:19:23 +0300

    added drawing on canvas on mouse click for defenders

--- script.js
+++ script.js
@@ -7,6 +7,25 @@ canvas.height = 600;
 const cellSize = 100; //size of each cell in grid(100 x100)
 const cellGap = 3;
 const gameGrid = [];
+const defenders = [];
+let numberOfResources = 300;
+//mouse- drawing cells when mouse hovers over it
+const mouse ={
+    x: 10,
+    y: 10,
+    width: 0.1,
+    height: 0.1,
+}
+let canvasPosition = canvas.getBoundingClientRect();
+canvas.addEventListener('mousemove', function(e){
+   mouse.x = e.x - canvasPosition.left;
+   mouse.y = e.y - canvasPosition.top;
+});
+
+canvas.addEventListener('mouseleave', function(){
+    mouse.y = undefined;
+    mouse.y = undefined;
+})
 
 //gameboard
 const constrolsBar = {
@@ -22,9 +41,12 @@ class Cell{
         this.height = cellSize;
 }
 draw(){ //draws the cell as a black rectangle on the canvas
-    ctx.strokeStyle = 'black';
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
-}
+    if(mouse.x && mouse.y && collision(this, mouse)){
+        ctx.strokeStyle = 'black';
+        ctx.strokeRect(this.x, this.y, this.width, this.height);
+    }
+    
+    }
 }
 
 function createGrid(){
@@ -45,14 +67,68 @@ function handleGameGrid(){
 
 //projectiles
 //defenders
+class Defender{
+    constructor(x, y){
+        this.x = x;
+        this.y = y;
+        this.width = cellSize;
+        this.height = cellSize;
+        this.shooting = false;//shoot only when enemy is detected on row
+        this.health = 100;
+        this.projectiles = [];
+        this.timer = 0;
+    }
+    draw(){
+        ctx.fillStyle = 'blue';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = 'gold';
+        ctx.font = '20px Arial';
+        ctx.fillText(Math.floor(this.health), this.x, this.y);//to give health in whole numbers
+
+
+    }
+}
+//creating new defender on canvas after clicking
+canvas.addEventListener('click', function(){
+const gridPositonX = mouse.x - (mouse.x % cellSize);
+const gridPositonY = mouse.y - (mouse.y % cellSize);
+if (gridPositonY < cellSize) return;//to ensure you cant add a defender on the top blue spot
+let defenderCost = 100;
+if (numberOfResources > defenderCost){
+    defenders.push(new Defender(gridPositonX, gridPositonY));
+    numberOfResources -= defenderCost;
+}
+
+
+}) ;
+function handleDefenders(){
+    for (let i = 0; i < defenders.length; i++){
+        defenders[i].draw();
+    }
+}
 //enemies
 //utilities
 
 function animate() { //recursion to redraw the animation over and over
+    ctx.clearRect(0, 0, canvas.width, canvas.height);//draws only current cell being hovered on
     ctx.fillStyle = 'blue';
     ctx.fillRect(0, 0,constrolsBar.width, constrolsBar.height);
     handleGameGrid();
+    handleDefenders();
     requestAnimationFrame(animate);
 }
 
 animate();
+
+function collision(first, second){
+    if(
+        !(first.x > second.x + second.width ||
+        first.x + first.width < second.x ||
+        first.y > second.y + second.height ||   
+        first.y + first.height < second.y)
+
+        ) {
+            return true;
+        };
+    
+};
