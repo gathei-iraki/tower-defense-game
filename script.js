ecea3b9eb30d64fca4b0f58a3453b9ca81d26f2e
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-11-06 12:44:10 +0300

    added health,projectiles,score

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ canvas.height = 600;
 //global variables
 const cellSize = 100; //size of each cell in grid(100 x100)
 const cellGap = 3;
-const gameGrid = [];
-const defenders = [];
 let numberOfResources = 300;
-const enemies = [];
-const enemyPositions = [];
 let enemiesInterval = 600;
 let frame = 0;
 let gameOver = false;
+let score = 0;
+
+const gameGrid = [];
+const defenders = [];
+const enemies = [];
+const enemyPositions = [];
+const projectiles =[];
 //mouse- drawing cells when mouse hovers over it
 const mouse ={
     x: 10,
@@ -71,13 +74,66 @@ function handleGameGrid(){
 }
 
 //projectiles
+class Projectile {
+constructor(x,y){
+    this.x = x;
+    this.y = y;
+    this.width = 10;
+    this.height = 10;
+    this.power = 20;
+    this.speed = 5;
+
+
+}
+update(){
+    this.x += this.speed;
+
+}
+draw(){
+    ctx.fillStyle = 'black' ;
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.width, 0, Math.PI * 2 );
+    ctx.fill();
+}
+}
+
+//to allow shooting if you have enough ammo
+function handleProjectiles()    {
+    for (let i = 0; i < projectiles.length; i++){
+    projectiles[i].update();
+    projectiles[i].draw();
+
+//to reduce health of enemy once it collides with projectiles
+for (let j =0; j < enemies.length; j++){
+    if(enemies[j] && projectiles[i] && collision(projectiles[i], enemies[j])){
+        enemies[j].health -= projectiles[i].power;
+
+        //removing projectile from array after it hits enemy
+        projectiles.splice(i, 1);
+        i--;
+    }
+
+}
+
+
+    //making sure projectiles are fired within the canvas
+    if (projectiles[i] && projectiles[i].x > canvas.width - cellSize){
+        projectiles.splice(i, 1);
+        i--;
+
+    }
+
+    }
+}
+
+
 //defenders
 class Defender{
     constructor(x, y){
         this.x = x;
         this.y = y;
-        this.width = cellSize;
-        this.height = cellSize;
+        this.width = cellSize - cellGap * 2;
+        this.height = cellSize - cellGap * 2;
         this.shooting = false;//shoot only when enemy is detected on row
         this.health = 100;
         this.projectiles = [];
@@ -92,11 +148,23 @@ class Defender{
 
 
     }
+    update(){
+        if (this.shooting){
+            this.timer++;
+            if (this.timer % 100 === 0){
+                projectiles.push(new Projectile(this.x + 70, this.y + 50)); //to ensure lasers originate from the defender who fires them
+    
+            }
+        }else {
+            this.timer = 0;
+        }
+       
+    }
 }
 //creating new defender on canvas after clicking
 canvas.addEventListener('click', function(){
-const gridPositonX = mouse.x - (mouse.x % cellSize);
-const gridPositonY = mouse.y - (mouse.y % cellSize);
+const gridPositonX = mouse.x - (mouse.x % cellSize) + cellGap;
+const gridPositonY = mouse.y - (mouse.y % cellSize) + cellGap;
 if (gridPositonY < cellSize) return;//to ensure you cant add a defender on the top blue spot
 //to avoid placing a defender on top of another
 for (let i = 0; i < defenders.length; i++){
@@ -114,9 +182,22 @@ if (numberOfResources >= defenderCost){
 function handleDefenders(){
     for (let i = 0; i < defenders.length; i++){
         defenders[i].draw();
+        defenders[i].update();
+
+        //to check if defender has an enemy on its row
+        if (enemyPositions.indexOf(defenders[i].y) !== -1){
+            defenders[i].shooting = true;
+
+        }else{
+            defenders[i].shooting = false;
+
+        }
+
+
+
         //checks collision between defender and any enemy object
         for (let j = 0; j < enemies.length; j++){
-            if (collision(defenders[i], enemies[j])){
+            if (defenders[i] && collision(defenders[i], enemies[j])){
                 enemies[j].movement = 0; //setting enemy movement to 0
                 defenders[i].health -= 0.2;
 
@@ -165,6 +246,17 @@ for (let i = 0; i < enemies.length; i++){
         gameOver = true;
     }
 
+    //remove enemy once health reaches 0
+    if (enemies[i].health <= 0){
+        let gainedResources = enemies[i].maxHealth/10;
+        numberOfResources += gainedResources;
+        score += gainedResources;
+        const findThisIndex = enemyPositions.indexOf(enemies[i].y);
+        enemyPositions.splice(findThisIndex, 1);
+        enemies.splice(i, 1);
+        i--;
+    }
+
 }
 if (frame % enemiesInterval === 0){ //every 600 frames new enemy will be added
     let verticalPosition = Math.floor(Math.random() * 5 + 1) * cellSize;
@@ -177,7 +269,8 @@ if (frame % enemiesInterval === 0){ //every 600 frames new enemy will be added
 function handleGameStatus(){
     ctx.fillStyle = 'gold';
     ctx.font = '30px Orbitron';
-    ctx.fillText('Resources: ' + numberOfResources, 20, 55);
+    ctx.fillText('Score: ' + score, 20, 40);
+    ctx.fillText('Resources: ' + numberOfResources, 20, 80);
     if (gameOver){
         ctx.fillStyle = 'black';
         ctx.font = '90px Orbitron';
@@ -193,6 +286,7 @@ function animate() { //recursion to redraw the animation over and over
     ctx.fillRect(0, 0,constrolsBar.width, constrolsBar.height);
     handleGameGrid();
     handleDefenders();
+    handleProjectiles();
     handleEnemies();
     handleGameStatus();
 frame++;
