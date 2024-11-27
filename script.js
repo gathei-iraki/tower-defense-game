11fd4ac5da4d23cdb8b567de027b38f27dae289a
gathei-njoroge <60644525+gathei-iraki@users.noreply.github.com>
2024-11-27 22:52:19 +0300

    added gun sync

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let frame = 0;
 let gameOver = false;
 let score = 0;
 const winningScore = 50;
-let chooseDefender = 1;
+let chosenDefender = 1;
 const gameGrid = [];
 const defenders = [];
 const enemies = [];
@@ -25,7 +25,16 @@ const mouse ={
     y: 10,
     width: 0.1,
     height: 0.1,
+    clicked: false
 }
+canvas.addEventListener('mousedown', function(){
+   mouse.clicked = true; 
+});
+
+canvas.addEventListener('mouseup', function(){
+    mouse.clicked = false; 
+
+});
 let canvasPosition = canvas.getBoundingClientRect();
 canvas.addEventListener('mousemove', function(e){
    mouse.x = e.x - canvasPosition.left;
@@ -92,7 +101,7 @@ update(){
 
 }
 draw(){
-    ctx.fillStyle = 'black' ;
+    ctx.fillStyle = 'gold' ;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.width, 0, Math.PI * 2 );
     ctx.fill();
@@ -151,14 +160,21 @@ class Defender{
         this.spriteHeight = 194;
         this.minFrame = 0;
         this.maxFrame = 16;
-    }
+        this.chosenDefender = chosenDefender;
+    }   
     draw(){
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        //ctx.fillStyle = 'blue';
+        //ctx.fillRect(this.x, this.y, this.width, this.height);
         ctx.fillStyle = 'gold';
         ctx.font = '20px Orbitron';
         ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);//to give health in whole numbers
-        ctx.drawImage(defender1, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        if (this.chosenDefender === 1){
+            ctx.drawImage(defender1, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+
+        }else if (this.chosenDefender === 2){
+            ctx.drawImage(defender2, this.frameX * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+
+        }
 
     }
     update(){
@@ -169,7 +185,7 @@ class Defender{
         }
         if (this.shooting){// switches frame to idle when no enemy is detected on row
            this.minFrame = 0;
-           this.maxFrame = 15; 
+           this.maxFrame = 16; 
         } else{
             this.minFrame = 17;
             this.maxFrame = 23;
@@ -216,9 +232,55 @@ function handleDefenders(){
         }
     }
 }
+//PLAYER CHOOSING CARD
+const card1 = 
+{
+  x: 10,
+  y: 10,
+  width: 70,
+  height: 85
+}
 
+const card2 = 
+{
+  x: 90,
+  y: 10,
+  width: 70,
+  height: 85
+}
 
+function chooseDefender(){
+    let card1stroke = 'black';
+    let card2stroke = 'black';
+if (collision(mouse, card1) && mouse.clicked){
+    chosenDefender = 1;
+ } else if (collision(mouse, card2) && mouse.clicked ){
+    chosenDefender = 2;
+ }
+ if (chosenDefender === 1){
+     card1stroke = 'gold';
+     card2stroke = 'black';
 
+ } else if (chosenDefender === 2){
+    card1stroke = 'black';
+    card2stroke = 'gold';
+ } else {
+    card1stroke = 'black';
+    card2stroke = 'black';
+ }
+
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'rgba(0,0,0,0.2)';
+    ctx.fillRect(card1.x, card1.y, card1.width, card1.height);
+    ctx.strokeStyle = card1stroke;
+    ctx.strokeRect(card1.x, card1.y, card1.width, card1.height);
+    ctx.drawImage(defender1, 0, 0, 194, 194, 0, 5, 194/2, 194/2);
+    ctx.fillRect(card2.x, card2.y, card2.width, card2.height);
+    ctx.drawImage(defender2, 0, 0, 194, 194, 80, 5, 194/2, 194/2);
+    ctx.strokeStyle = card2stroke;
+    ctx.strokeRect(card2.x, card1.y, card1.width, card1.height);
+
+}
 //floating Messages
 const floatingMessages = [];
 class floatingMessage{
@@ -296,7 +358,7 @@ class Enemy{
     draw(){
         //ctx.fillStyle = 'red';
         //ctx.fillRect(this.x, this.y, this.width, this.height);
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = 'gold';
         ctx.font = '20px Orbitron';
         ctx.fillText(Math.floor(this.health), this.x + 15, this.y + 30);//to give health in whole numbers
 
@@ -321,7 +383,7 @@ for (let i = 0; i < enemies.length; i++){
     if (enemies[i].health <= 0){
         let gainedResources = enemies[i].maxHealth/10;
         floatingMessages.push(new floatingMessage('+' + gainedResources, enemies[i].x, enemies[i].y, 30, 'black'));
-        floatingMessages.push(new floatingMessage('+' + gainedResources, 250, 50, 30, 'gold'));
+        floatingMessages.push(new floatingMessage('+' + gainedResources, 470, 85, 30, 'gold'));
 
         numberOfResources += gainedResources;
         score += gainedResources;
@@ -370,7 +432,7 @@ function handleResources(){
     if (resources[i] && mouse.x && mouse.y && collision(resources[i],mouse)){
         numberOfResources += resources[i].amount;
         floatingMessages.push(new floatingMessage('+' + resources[i].amount, resources[i].x, resources[i].y, 30, 'black'));
-        floatingMessages.push(new floatingMessage('+' + resources[i].amount, 250, 50, 30, 'gold'));
+        floatingMessages.push(new floatingMessage('+' + resources[i].amount, 470, 85, 30, 'gold'));
 
         resources.splice(i, 1);
         i--;
@@ -384,7 +446,7 @@ function handleGameStatus(){
     ctx.fillText('Score: ' + score, 180, 40);
     ctx.fillText('Resources: ' + numberOfResources, 180, 80);
     if (gameOver){
-        ctx.fillStyle = 'black';
+        ctx.fillStyle = 'gold';
         ctx.font = '90px Orbitron';
         ctx.fillText('GAME OVER', 135, 330);
 
@@ -429,6 +491,7 @@ function animate() { //recursion to redraw the animation over and over
     handleResources();
     handleProjectiles();
     handleEnemies();
+    chooseDefender();
     handleGameStatus();
     handleFloatingMessages();
 frame++;
